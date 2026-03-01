85ec81b3d319556b7b3c04d99b73c055546f9e3a
vyasaman26 <namasayv@gmail.com>
2026-03-01 23:38:58 +0530

    logout button added,edit and delete button will be gone when logged out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,36 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import '../style/Navbar.css'
+import { NavLink, replace, useNavigate } from "react-router-dom";
+import "../style/Navbar.css";
 import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
-  const {logout}=useContext(AuthContext)
+  const { logout, token } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login", { replace: true });
+  };
   return (
-   <nav className="navbar">
-  <div className="nav-container">
-    <h2 className="logo">BlogSpace</h2>
+    <nav className="navbar">
+      <div className="nav-container">
+        <h2 className="logo">BlogSpace</h2>
 
-    <div className="nav-links">
-      <NavLink to="/" className="nav-link">
-        Home
-      </NavLink>
+        <div className="nav-links">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
 
-      <NavLink to="/create" className="nav-link">
-        Create
-      </NavLink>
-      <p className="nav-link" onClick={logout}>Logout</p>
-    </div>
-  </div>
-</nav>
+          <NavLink to="/create" className="nav-link">
+            Create
+          </NavLink>
+          {token && (
+            <button className="nav-link logout-btn" onClick={handleLogout}>
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -2,58 +2,56 @@ import React from "react";
 import { useContext } from "react";
 import { BlogContext } from "../context/BlogContext";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import "../style/BlogDetails.css"
+import "../style/BlogDetails.css";
+import { AuthContext } from "../context/AuthContext";
 
 const BlogDetails = () => {
-  const Navigate=useNavigate()
+  const Navigate = useNavigate();
   const { id } = useParams();
-  const { blogs,setBlogs } = useContext(BlogContext);
-  const blog=blogs.find((b)=>b.id===Number(id))
+  const { blogs, setBlogs } = useContext(BlogContext);
+  const blog = blogs.find((b) => b.id === Number(id));
+  const { token } = useContext(AuthContext);
 
-  const handleDelete=()=>{
-    const filteredBlog=blogs.filter(
-      (b)=>b.id!==Number(id)
-    )
-    setBlogs(filteredBlog)
-    Navigate("/")
-  }
+  const handleDelete = () => {
+    const filteredBlog = blogs.filter((b) => b.id !== Number(id));
+    setBlogs(filteredBlog);
+    Navigate("/");
+  };
   return (
-  <div className="details-wrapper">
-  <div className="details-card">
+    <div className="details-wrapper">
+      <div className="details-card">
+        <div className="details-header">
+          <h1 className="details-title">{blog.title}</h1>
 
-    <div className="details-header">
-      <h1 className="details-title">{blog.title}</h1>
+          <div className="details-meta">
+            <span>By Aman Vyas</span>
+            <span>•</span>
+            <span>{new Date(blog.date).toDateString()}</span>
+            <span>•</span>
+            <span className="reading-time">
+              {Math.ceil(blog.content.split(" ").length / 200)} min read
+            </span>
+          </div>
+        </div>
 
-      <div className="details-meta">
-        <span>By Aman Vyas</span>
-        <span>•</span>
-        <span>{new Date(blog.date).toDateString()}</span>
-        <span>•</span>
-        <span className="reading-time">
-          {Math.ceil(blog.content.split(" ").length / 200)} min read
-        </span>
+        <hr className="details-divider" />
+
+        <div className="details-body">
+          <p>{blog.content}</p>
+        </div>
+        {token && (
+          <div className="details-actions">
+            <Link to={`/edit/${blog.id}`} className="btn-edit">
+              ✏ Edit
+            </Link>
+
+            <button onClick={handleDelete} className="btn-delete">
+              🗑 Delete
+            </button>
+          </div>
+        )}
       </div>
     </div>
-
-    <hr className="details-divider" />
-
-    <div className="details-body">
-      <p>{blog.content}</p>
-    </div>
-
-    <div className="details-actions">
-      <Link to={`/edit/${blog.id}`} className="btn-edit">
-        ✏ Edit
-      </Link>
-
-      <button onClick={handleDelete} className="btn-delete">
-        🗑 Delete
-      </button>
-    </div>
-
-  </div>
-</div>
-    
   );
 };
 
